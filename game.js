b302e40dbda887bb24a933f905130be58e742885
Kenson Johnson <kenson.johnson@outlook.com>
2022-02-19 21:35:53 -0600

    Polished user interface
    Added a score box for both player and computer.
    The round count now updates with each round.

--- game.js
+++ game.js
@@ -10,7 +10,6 @@ as well as the generated computer choice. The choices are
 compared and a winner is chosen. Have this return
 the results as a string that declares the player as a winner,
 a loser, or that the game tied. */
-
 function playRound(playerChoice) {
 
     //create the computer's choice
@@ -20,25 +19,77 @@ function playRound(playerChoice) {
     if (playerChoice == computerChoice) {
         return 'Its a tie! Play again!';
     } else if (playerChoice == 'rock' && computerChoice == 'scissors') {
+        playerWin();
         return 'You win! Rock beats scissors.';
     } else if (playerChoice == 'rock' && computerChoice == 'paper') {
+        computerWin();
         return 'You lose! Paper beats rock.';
     } else if (playerChoice == 'scissors' && computerChoice == 'paper') {
+        playerWin();
         return 'You win! Scissors beats paper.';
     } else if (playerChoice == 'scissors' && computerChoice == 'rock') {
+        computerWin();
         return 'You lose! Rock beats scissors.';
     } else if (playerChoice == 'paper' && computerChoice == 'rock') {
+        playerWin();
         return 'You win! Paper beats rock.';
     } else if (playerChoice == 'paper' && computerChoice == 'scissors') {
+        computerWin();
         return 'You lose! Scissors beats paper.'
     }
 
 }
 
+//set round counter variable
+let roundCount = 0;
+
+
+//create function to update round count and diplay on screen
+function updateRound() {
+    roundCount++;
+    document.getElementById("round-count").innerHTML = roundCount;
+}
+
+
+
+//set variable to hold player score
+let playerScore = 0;
+
+//create function that increments the player score and updates on screen
+function playerWin() {
+    playerScore++
+    document.getElementById("player-score").innerHTML = playerScore;
+
+}
+
+
+
+//set variable to hold computer score
+let computerScore = 0;
+
+//create function that increments the computer score and updates on screen
+function computerWin() {
+    computerScore++
+    document.getElementById("computer-score").innerHTML = computerScore;
+}
+
+
+
+
+
+
+
 //take in the player's choice and play a round, updating the score if possible
 function game(playerSelection) {
 
     results = playRound(playerSelection);
+    updateRound();
+    if (playerScore >= 5) {
+        endGame(true);
+    }
+    if (computerScore >= 5) {
+        endGame(false);
+    }
 
 
     //update results on screen
